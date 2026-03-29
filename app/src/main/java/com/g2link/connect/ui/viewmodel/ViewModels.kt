bc84e117428667718ff0dbc9ec3d131ca3453d91
G2-links <henatabassummp@gmail.com>
2026-03-29 10:48:50 +0300

    Create ViewModels.kt

---
+++ app/src/main/java/com/g2link/connect/ui/viewmodel/ViewModels.kt
@@ -0,0 +1,8 @@
+package com.g2link.connect.ui.viewmodel // UPDATED
+
+import androidx.lifecycle.ViewModel
+import com.g2link.connect.service.MeshForegroundService // UPDATED
+// ... other imports ...
+
+// Ensure all other imports in this file starting with 
+// com.disastermesh are changed to com.g2link
